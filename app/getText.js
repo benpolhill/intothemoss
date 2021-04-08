b54e56e427cd78c04fe97d7a97c59d7f0a0e2005
Ben Polhill <benpolhill@gmail.com>
2021-04-08 18:15:20 +0100

    Remove getText file

--- app/getText.js
+++
@@ -1,9 +0,0 @@
-async function getText() {
-
-    const textFiles = await fetch(`/episodes/text/016.txt`);
-    const text = await textFiles.text();
-    // console.log(text);
-    return text;
-}
-
-export default getText();
