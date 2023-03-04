7347b31a53db6678a2cc3422c95f62fdea2fb547
Ben Polhill <benpolhill@gmail.com>
2023-03-04 11:47:55 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROOT_DIR=`pwd`
+IMGS_DIR=assets/images
 EP_ID=$1
 if [ -z "$1" ]; then
     echo "Episode number? [0xx]:"
@@ -8,14 +9,14 @@ if [ -z "$1" ]; then
 fi
 
 sh buildXML.sh
-cd episodes/images
+cd $IMGS_DIR
 cwebp -resize 1000 1000 -q 50 jpg/1400/$EP_ID.jpg -o webp/1000/$EP_ID.webp
 cwebp -resize 150 150 -q 50 jpg/1400/$EP_ID.jpg -o webp/150/$EP_ID.webp
 cwebp -resize 300 300 -q 50 jpg/1400/$EP_ID.jpg -o webp/300/$EP_ID.webp
 convert jpg/1400/$EP_ID.jpg -resize 300x300 -quality 60 jpg/300/$EP_ID.jpg
 convert jpg/1400/$EP_ID.jpg -resize 300x300 -quality 60 jpg/150/$EP_ID.jpg
 cd $ROOT_DIR
-echo "Syncing episodes/audio dir..."
+echo "Syncing audio dir..."
 rsync -arv audio/ root@dept2.co:/var/www/intothemoss.co.uk/audio
 npm run build
 git add .
