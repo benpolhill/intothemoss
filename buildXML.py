6832657304cdb53eca55eed6a3aad25f999b0d26
Ben Polhill <benpolhill@gmail.com>
2023-05-27 15:35:47 +0100

    Refactor code and add error handling

--- buildXML.py
+++ buildXML.py
@@ -6,110 +6,135 @@ from mutagen.id3 import ID3
 from mutagen.mp3 import MP3
 from xml.dom import minidom
 
-# Identify the most recent file
-latest_file = sorted(glob.glob('./audio/*.mp3'))[-1]
+def get_latest_file():
+    return sorted(glob.glob('./audio/*.mp3'))[-1]
 
-# Use ID3 instead of EasyID3 to access non-standard tags
-info = ID3(latest_file)
+def get_file_info(latest_file):
+    return ID3(latest_file)
 
-# Load the MP3 file
-audio = MP3(latest_file)
+def get_audio_info(latest_file):
+    return MP3(latest_file)
 
-# Convert TGID into a datetime object
+def get_file_length(latest_file):
+    return str(os.path.getsize(latest_file))
+
 def tgid_to_datetime(tgid):
-    date_str = tgid[3:]  # Get the date part from the TGID
-    date = datetime.datetime.strptime(date_str, "%Y%m%d")  # Convert to a datetime object
-    date = date.replace(hour=18, minute=30)  # Set the time to 18:30
-    date = date.replace(tzinfo=datetime.timezone.utc)  # Make it a timezone-aware datetime object
+    date_str = tgid[3:]
+    date = datetime.datetime.strptime(date_str, "%Y%m%d")
+    date = date.replace(hour=18, minute=30)
+    date = date.replace(tzinfo=datetime.timezone.utc)
     return date
 
-episode = info.get('TRCK').text[0] if info.get('TRCK') else info.get('track').text[0] if info.get('track') else ''
-episode_id = latest_file.split('/')[-1].replace('.mp3', '')
-season = info.get('TPOS').text[0] if info.get('TPOS') else ''
-title = info.get('TIT2').text[0] if info.get('TIT2') else ''
-desc = info.get('TDES').text[0] if info.get('TDES') else ''
-url = f"https://intothemoss.com/audio/{episode_id}.mp3"
-length_int = os.path.getsize(latest_file)
-length = str(length_int)
-image = f"https://intothemoss.com/assets/images/jpg/1400/{episode_id}.jpg"
-link = "https://intothemoss.com/episodes/" + episode_id
-guid = info.get('TGID').text[0] if info.get('TGID') else ''
-pubdate = tgid_to_datetime(info['TGID'][0]).strftime("%a, %d %b %Y %H:%M:%S %z")
-duration = audio.info.length
-# Round to two decimal places and convert to a string
-duration = "{:.2f}".format(duration)
-explicit = "false"
+def create_new_item(file_info, namespaces, latest_file, audio_info, episode, season, title, desc, url, length, image, link, guid, pubdate, duration, explicit):
+    new_item = ET.Element('item')
 
-# parse xml
-tree = ET.parse('./feed.xml')
-root = tree.getroot()
+    new_item.append(create_element(namespaces['itunes'], 'episode', episode))
+    new_item.append(create_element(namespaces['itunes'], 'season', season))
+    new_item.append(create_element(None, 'title', title))
+    new_item.append(create_element(None, 'description', desc))
 
-# define and register namespaces
-namespaces = {'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
-for prefix, uri in namespaces.items():
-    ET.register_namespace(prefix, uri)
+    enclosure_tag = ET.Element('enclosure')
+    enclosure_tag.set('url', url)
+    enclosure_tag.set('length', length)
+    enclosure_tag.set('type', "audio/mpeg")
+    new_item.append(enclosure_tag)
 
-# create a new item
-new_item = ET.Element('item')
+    new_item.append(create_element(None, 'link', link))
+    new_item.append(create_image(namespaces['itunes'], image))
+    new_item.append(create_element(None, 'guid', guid))
+    new_item.append(create_element(None, 'pubDate', pubdate))
+    new_item.append(create_element(namespaces['itunes'], 'duration', duration))
+    new_item.append(create_element(namespaces['itunes'], 'explicit', explicit))
 
-episode_tag = ET.Element(ET.QName(namespaces['itunes'], 'episode'))
-episode_tag.text = episode
-new_item.append(episode_tag)
+    return new_item
 
-season_tag = ET.Element(ET.QName(namespaces['itunes'], 'season'))
-season_tag.text = season
-new_item.append(season_tag)
+def create_element(namespace, tag, text):
+    element = ET.Element(ET.QName(namespace, tag))
+    element.text = text
+    return element
 
-title_tag = ET.Element('title')
-title_tag.text = title
-new_item.append(title_tag)
+def create_enclosure(url, length):
+    enclosure = ET.Element('enclosure')
+    enclosure.set('url', url)
+    enclosure.set('length', length)
+    enclosure.set('type', "audio/mpeg")
+    return enclosure
 
-desc_tag = ET.Element('description')
-desc_tag.text = desc
-new_item.append(desc_tag)
+def create_image(namespace, image):
+    image_tag = ET.Element(ET.QName(namespace, 'image'))
+    image_tag.set('href', image)
+    return image_tag
 
-enclosure_tag = ET.Element('enclosure')
-enclosure_tag.set('url', url)
-enclosure_tag.set('length', length)
-enclosure_tag.set('type', "audio/mpeg")
-new_item.append(enclosure_tag)
+def check_item_exists(root, url):
+    for item in root.iter('item'):
+        enclosure = item.find('enclosure')
+        if enclosure is not None and enclosure.get('url') == url:
+            return True
+    return False
 
-link_tag = ET.Element('link')
-link_tag.text = link
-new_item.append(link_tag)
+def update_xml_tree(root, new_item, url):
+    if check_item_exists(root, url):
+        print(f"An item with URL {url} already exists in the feed.")
+        return
+    # find the channel element
+    for channel in root.iter('channel'):
+        # find the first item index in the channel
+        first_item_index = next(i for i, child in enumerate(channel) if child.tag == 'item')
+        # insert the new item before the first existing item in the channel
+        channel.insert(first_item_index, new_item)
+        break
 
-image_tag = ET.Element(ET.QName(namespaces['itunes'], 'image'))
-image_tag.set('href', image)
-new_item.append(image_tag)
+def write_to_file(root, filename):
+    pretty_xml = minidom.parseString(ET.tostring(root, encoding='utf-8')).toprettyxml(indent="    ")
+    pretty_xml = '\n'.join([s for s in pretty_xml.splitlines() if s.strip()]) # remove empty lines
+    with open(filename, "w") as f:
+        f.write(pretty_xml)
 
-guid_tag = ET.Element('guid')
-guid_tag.text = guid
-new_item.append(guid_tag)
+def main():
+    # Identify the most recent file
+    latest_file = get_latest_file()
 
-pubDate_tag = ET.Element('pubDate')
-pubDate_tag.text = pubdate
-new_item.append(pubDate_tag)
+    # Use ID3 instead of EasyID3 to access non-standard tags
+    info = get_file_info(latest_file)
 
-duration_tag = ET.Element(ET.QName(namespaces['itunes'], 'duration'))
-duration_tag.text = duration
-new_item.append(duration_tag)
+    # Load the MP3 file
+    audio = get_audio_info(latest_file)
 
-explicit_tag = ET.Element(ET.QName(namespaces['itunes'], 'explicit'))
-explicit_tag.text = explicit
-new_item.append(explicit_tag)
+    # Get information from the file
+    episode = info.get('TRCK').text[0] if info.get('TRCK') else info.get('track').text[0] if info.get('track') else ''
+    episode_id = latest_file.split('/')[-1].replace('.mp3', '')
+    season = info.get('TPOS').text[0] if info.get('TPOS') else ''
+    title = info.get('TIT2').text[0] if info.get('TIT2') else ''
+    desc = info.get('TDES').text[0] if info.get('TDES') else ''
+    url = f"https://intothemoss.com/audio/{episode_id}.mp3"
+    length = get_file_length(latest_file)
+    image = f"https://intothemoss.com/assets/images/jpg/1400/{episode_id}.jpg"
+    link = "https://intothemoss.com/episodes/" + episode_id
+    guid = info.get('TGID').text[0] if info.get('TGID') else ''
+    pubdate = tgid_to_datetime(info['TGID'][0]).strftime("%a, %d %b %Y %H:%M:%S %z")
+    duration = audio.info.length
+    # Round to two decimal places and convert to a string
+    duration = "{:.2f}".format(duration)
+    explicit = "false"
 
-# find the channel element
-for channel in root.iter('channel'):
-    # find the first item index in the channel
-    first_item_index = next(i for i, child in enumerate(channel) if child.tag == 'item')
-    # insert the new item before the first existing item in the channel
-    channel.insert(first_item_index, new_item)
-    break
+    # define and register namespaces
+    namespaces = {'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+    for prefix, uri in namespaces.items():
+        ET.register_namespace(prefix, uri)
 
-# write the new xml to file
-pretty_xml = minidom.parseString(ET.tostring(root, encoding='utf-8')).toprettyxml(indent="    ")
-pretty_xml = '\n'.join([s for s in pretty_xml.splitlines() if s.strip()]) # remove empty lines
+    # create a new item
+    new_item = create_new_item(info, namespaces, latest_file, audio, episode, season, title, desc, url, length, image, link, guid, pubdate, duration, explicit)
 
-with open("feed.xml", "w") as f:
-    f.write(pretty_xml)
+    # parse xml
+    tree = ET.parse('./feed.xml')
+    root = tree.getroot()
 
+    # update XML tree with the new item
+    update_xml_tree(root, new_item, url)
+
+    # write the new xml to file
+    write_to_file(root, "feed.xml")
+
+
+if __name__ == "__main__":
+    main()
