9e11402bc48fac6baee11e6047b9705e169b8375
Ben Polhill <benpolhill@gmail.com>
2024-08-11 20:43:42 +0100

    deploy episode 169

--- deploy.sh
+++ deploy.sh
@@ -121,3 +121,11 @@ cwebp -resize 150 150 -q 50 jpg/1400/$EP_ID.jpg -o webp/150/$EP_ID.webp
 cwebp -resize 300 300 -q 50 jpg/1400/$EP_ID.jpg -o webp/300/$EP_ID.webp
 convert jpg/1400/$EP_ID.jpg -resize 300x300 -quality 60 jpg/300/$EP_ID.jpg
 convert jpg/1400/$EP_ID.jpg -resize 300x300 -quality 60 jpg/150/$EP_ID.jpg
+
+cd $ROOT_DIR
+echo "Syncing audio dir..."
+rsync -arv audio/ root@dept2.co:/var/www/intothemoss/audio
+npm run build
+git add .
+git commit -m "deploy episode $EP_ID"
+git push
